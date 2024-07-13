54d5c52eb791a259d4619ee5c0229c990b17151e
draconivis <patrick.gasser@proton.me>
2024-07-13 16:51:45 +0200

    moved over config changes from home-manager and old dotfiles repo; switched to oh-my-posh; removed some zsh plugins; moved back to catppuccin

--- dotfiles/zsh/README.md
+++ dotfiles/zsh/README.md
@@ -2,15 +2,10 @@
 
 ## setup
 
-### [antigen](https://github.com/zsh-users/antigen)
-
-1. install antigen
+1. install [antidote](https://github.com/mattmc3/antidote)
+2. link the [zshrc](/configs/dotfiles/zsh/zshrc) to your home dir
   ```sh
-  curl -L git.io/antigen > ${HOME}/.config/zsh/antigen.zsh
-  ```
-2. link the correct [.zshrc](/configs/dotfiles/zsh/zshrc-antigen)
-  ```sh
-  ln -s ${HOME}/.config/zsh/zshrc-antigen ${HOME}/.zshrc
+  ln -s ${HOME}/.config/zsh/zshrc ${HOME}/.zshrc
   ```
 3. restart zsh
-  open a new zsh instance and antigen will automatically install the plugins
+  open a new zsh instance and antidote will automatically install the plugins
--- dotfiles/zsh/zsh_plugins.zsh
+++ dotfiles/zsh/zsh_plugins.zsh
@@ -1,17 +1,14 @@
-fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zephyr/plugins/completion )
-source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zephyr/plugins/completion/completion.plugin.zsh
-fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ael-code-SLASH-zsh-colored-man-pages )
-source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-ael-code-SLASH-zsh-colored-man-pages/colored-man-pages.plugin.zsh
 fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-davidde-SLASH-git )
 source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-davidde-SLASH-git/git.plugin.zsh
-fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
-source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel10k.zsh-theme
-source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k/powerlevel9k.zsh-theme
-fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting )
-source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-continuum-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-marlonrichert-SLASH-zsh-autocomplete )
+source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-marlonrichert-SLASH-zsh-autocomplete/zsh-autocomplete.plugin.zsh
+fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+if ! (( $+functions[zsh-defer] )); then
+  fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer )
+  source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-zsh-defer/zsh-defer.plugin.zsh
+fi
 fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+zsh-defer source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
 source /home/patrick/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
