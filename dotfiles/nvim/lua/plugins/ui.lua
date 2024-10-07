29159dd1c31b50113c9f30c3b6e6eb730efde034
draconivis <patrick.gasser@proton.me>
2024-10-07 09:14:04 +0200

    nvim: trying out new colorschemes

--- dotfiles/nvim/lua/plugins/ui.lua
+++ dotfiles/nvim/lua/plugins/ui.lua
@@ -29,16 +29,56 @@ return {
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("catppuccin-mocha")
+			-- vim.cmd.colorscheme("catppuccin-mocha")
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
 		end,
 	},
-	{ -- indentation guides
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
+	{
+		"rebelot/kanagawa.nvim",
+		init = function()
+			require("kanagawa").setup({
+				colors = {
+					theme = {
+						all = {
+							ui = {
+								bg_gutter = "none",
+							},
+						},
+					},
+				},
+				overrides = function(colors)
+					local theme = colors.theme
+					return {
+						NormalFloat = { bg = "none" },
+						FloatBorder = { bg = "none" },
+						FloatTitle = { bg = "none" },
+
+						-- Save an hlgroup with dark background and dimmed foreground
+						-- so that you can use it where your still want darker windows.
+						-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+						NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+						-- Popular plugins that open floats will link to NormalFloat by default;
+						-- set their background accordingly if you wish to keep them dark and borderless
+						LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+						MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+					}
+				end,
+			})
+			vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
 		opts = {},
+		init = function()
+			-- vim.cmd.colorscheme("tokyonight-night")
+		end,
 	},
 	-- Highlight todo, notes, etc in comments
 	{
