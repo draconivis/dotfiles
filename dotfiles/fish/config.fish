251b991194b83eed5e04db10ac93933c30dbcbd6
draconivis <patrick.gasser@proton.me>
2025-12-10 13:20:18 +0100

    fish: more functions & config update

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,6 +1,9 @@
-set -g fish_greeting
 set FISH_HOME $HOME/.config/fish
+set -g fish_greeting
+set -g PAGER delta
+set -g EDITOR vim
 fish_add_path /opt/homebrew/bin
+fish_add_path ~/.local/bin
 
 if test -f (brew --prefix)/etc/brew-wrap.fish
   source (brew --prefix)/etc/brew-wrap.fish
@@ -8,3 +11,4 @@ end
 
 oh-my-posh init fish --config $FISH_HOME/omp.toml | source
 zoxide init --cmd cd fish | source
+phpactor completion fish | source
